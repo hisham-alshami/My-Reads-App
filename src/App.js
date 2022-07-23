d04cadbc947a3bee3ade98262d6796c3e3c32e69
hisham-alshami <95663258+hisham-alshami@users.noreply.github.com>
2022-07-23 18:47:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route , Routes } from "react-router-dom";
 import MainPage from "./componant/MainPage";
 import SearchPage from "./componant/SeachPage";
 import * as BooksAPI from "./BooksAPI";
